e5cc5f8c44aa1ec15e296398f4df7e957a08bfe0
CCNV <huqichun@users.noreply.github.com>
2022-11-30 00:00:28 +0800

    Update XinQLOneKey.sh

--- XinQLOneKey.sh
+++ XinQLOneKey.sh
@@ -1,18 +1,17 @@
 #!/usr/bin/env bash
 
-TIME l "安装依赖..."
+echo "安装依赖..."
 echo
-TIME y "安装依赖需要时间，请耐心等待!"
-echo
-sleep 3
+echo "安装依赖需要时间，请耐心等待!"
 echo
 echo
-
+echo
 echo "当前node版本(如果没有node，请自行安装): "
 node -v
 
 echo "当前npm版本(如果没有npm，请自行安装): "
 npm -v
+sleep 5
 
 npm config set registry https://registry.npmmirror.com
 
@@ -36,10 +35,8 @@ cd /ql
 npm install -g npm png-js date-fns axios crypto-js ts-md5 tslib @types/node requests tough-cookie jsdom download tunnel fs ws form-data node-rsa jsencrypt ts-node typescript date-fns canvas
 
 
-echo
-TIME g "依赖安装完毕...建议重启 Docker "
+echo "依赖安装完毕...建议重启 Docker "
 
-echo
-TIME g "有任何问题，请在此仓库提交Issue： https://github.com/FlechazoPh/QLDependency"
+echo "有任何问题，请在此仓库提交Issue： https://github.com/FlechazoPh/QLDependency"
 echo
 exit 0
